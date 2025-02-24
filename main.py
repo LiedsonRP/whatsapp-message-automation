0c9c1a4fda74517a5f5af18e1ae984546936e9a8
LiedsonRP <liedsonreis2019@outlook.com>
2025-02-24 10:38:19 -0300

    criação de funções e adição de keys

--- main.py
+++ main.py
@@ -29,28 +29,29 @@ def create_contact_card(name : str, contact : str, id : str):
     ], border_width=1, relief=sg.RELIEF_SUNKEN, expand_x=True)]
 
 
-
-create_form_layout = [
+def create_insert_contact_form():
     
-    [sg.Text(text='Criar Contato', font=(TYPOGRAPHY_FONT, 18, 'bold') )],
-    [sg.HorizontalSeparator(color=SAVE_BUTTON_COLOR)],
+    return [
+    
+        [sg.Text(text='Criar Contato', font=(TYPOGRAPHY_FONT, 18, 'bold') )],
+        [sg.HorizontalSeparator(color=SAVE_BUTTON_COLOR)],
 
-    [sg.Text('Nome', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(10,2)), background_color=DEFAULT_WHITE,expand_x=True)],
-    [sg.Input(key='--NAME--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0, focus=True)],
+        [sg.Text('Nome', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(10,2)), background_color=DEFAULT_WHITE,expand_x=True)],
+        [sg.Input(key='--NAME--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0, focus=True)],
 
-    [sg.Text('Celular', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(18,2)), background_color=DEFAULT_WHITE, expand_x=True)],
-    [sg.Input(key='--CELLPHONE--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
+        [sg.Text('Celular', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(18,2)), background_color=DEFAULT_WHITE, expand_x=True)],
+        [sg.Input(key='--CELLPHONE--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
 
-    [sg.Text('Mensagem', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(18,2)), background_color=DEFAULT_WHITE, expand_x=True)],
-    [sg.Multiline(key='--MESSAGE--', background_color=(INPUT_COLOR), pad=((0,0),(0,20)), no_scrollbar=True, expand_y=True, font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
+        [sg.Text('Mensagem', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(18,2)), background_color=DEFAULT_WHITE, expand_x=True)],
+        [sg.Multiline(key='--MESSAGE--', background_color=(INPUT_COLOR), pad=((0,0),(0,20)), no_scrollbar=True, expand_y=True, font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
 
-    [
-        sg.Push(),
-        sg.Button(button_text='Salvar', button_color=(DEFAULT_TEXT_COLOR, SAVE_BUTTON_COLOR), mouseover_colors=HOVER_SAVE_BUTTON_COLOR, size=(15,2), border_width=0, key='--SAVE--'),
-        sg.Button(button_text='Cancelar', button_color=(DEFAULT_TEXT_COLOR, CANCEL_BUTTON_COLOR), size=(15,2), border_width=0, key='--CANCEL--'),
-        sg.Push(),
-    ],
-]
+        [
+            sg.Push(),
+            sg.Button(button_text='Salvar', button_color=(DEFAULT_TEXT_COLOR, SAVE_BUTTON_COLOR), mouseover_colors=HOVER_SAVE_BUTTON_COLOR, size=(15,2), border_width=0, key='--SAVE--'),
+            sg.Button(button_text='Cancelar', button_color=(DEFAULT_TEXT_COLOR, CANCEL_BUTTON_COLOR), size=(15,2), border_width=0, key='--CANCEL--'),
+            sg.Push(),
+        ],
+    ]
 
 list_contacts_layout = [
 
@@ -64,8 +65,8 @@ list_contacts_layout = [
 layout = [
 
     [
-        sg.Column(layout=list_contacts_layout, background_color=DEFAULT_WHITE, expand_x=True, expand_y=True,scrollable=True, vertical_scroll_only=True, sbar_relief=sg.RELIEF_FLAT),
-        sg.Column(layout=create_form_layout, background_color=DEFAULT_WHITE, expand_x=True, expand_y=True, p=((50,50), (20,20)))
+        sg.Column(layout=list_contacts_layout, background_color=DEFAULT_WHITE, expand_x=True, expand_y=True,scrollable=True, vertical_scroll_only=True, sbar_relief=sg.RELIEF_FLAT, key='--CONTACT_LIST--'),
+        sg.Column(layout=create_insert_contact_form(), background_color=DEFAULT_WHITE, expand_x=True, expand_y=True, p=((50,50), (20,20)), key='--RIGHT_SIDE--')
     ]
 ]
 
