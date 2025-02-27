a0a6b01990544dc9b897dbc37ff61d7872336855
LiedsonRP <liedsonreis2019@outlook.com>
2025-02-27 13:05:58 -0300

    Criação do update e modificação do lado direito

--- main.py
+++ main.py
@@ -15,18 +15,7 @@ GRAY = '#807676'
 
 TYPOGRAPHY_FONT ='./assets/RobotoCondensed-VariableFont_wght.ttf'
 
-contact_list = dict() #Dicionário de contatos
-
-def create_contact_card(name : str, contact : str, id : str):
-
-    return [sg.Frame(title="", layout=[
-        
-        [sg.Text(text=name, font=(TYPOGRAPHY_FONT, 14, 'normal') )],
-        [sg.Text(text='Ver contato', text_color=GRAY, font=(TYPOGRAPHY_FONT, 11, 'underline'), enable_events=True, key="--VER_CONTATO--")],
-
-
-
-    ], border_width=1, relief=sg.RELIEF_SUNKEN, expand_x=True)]
+contact_list = []
 
 main_layout = [
 
@@ -45,24 +34,46 @@ main_layout = [
     [sg.VPush()]
 ]
 
+update_contact_form = [
+    
+        [sg.Text(text='Editar Contato', font=(TYPOGRAPHY_FONT, 18, 'bold') )],
+        [sg.HorizontalSeparator(color=SAVE_BUTTON_COLOR)],
+
+        [sg.Text('Nome', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(10,2)), background_color=DEFAULT_WHITE,expand_x=True)],
+        [sg.Input(key='--NAME_UPDATE_FORM--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0, focus=True)],
+
+        [sg.Text('Celular', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(18,2)), background_color=DEFAULT_WHITE, expand_x=True)],
+        [sg.Input(key='--CELLPHONE_UPDATE_FORM--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
+
+        [sg.Text('Mensagem', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(18,2)), background_color=DEFAULT_WHITE, expand_x=True)],
+        [sg.Multiline(key='--MESSAGE_UPDATE_FORM--', background_color=(INPUT_COLOR), pad=((0,0),(0,20)), no_scrollbar=True, expand_y=True, font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
+
+        [
+            sg.Push(),
+            sg.Button(button_text='Salvar', button_color=(DEFAULT_TEXT_COLOR, SAVE_BUTTON_COLOR), mouseover_colors=HOVER_SAVE_BUTTON_COLOR, size=(15,2), border_width=0, key='--INSERT_SAVE--'),
+            sg.Button(button_text='Cancelar', button_color=(DEFAULT_TEXT_COLOR, CANCEL_BUTTON_COLOR), size=(15,2), border_width=0, key='--CANCEL_UPDATE--'),
+            sg.Push(),
+        ],
+    ]
+
 insert_contact_form = [
     
         [sg.Text(text='Criar Contato', font=(TYPOGRAPHY_FONT, 18, 'bold') )],
         [sg.HorizontalSeparator(color=SAVE_BUTTON_COLOR)],
 
         [sg.Text('Nome', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(10,2)), background_color=DEFAULT_WHITE,expand_x=True)],
-        [sg.Input(key='--NAME--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0, focus=True)],
+        [sg.Input(key='--NAME_INSERT_FORM--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0, focus=True)],
 
         [sg.Text('Celular', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(18,2)), background_color=DEFAULT_WHITE, expand_x=True)],
-        [sg.Input(key='--CELLPHONE--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
+        [sg.Input(key='--CELLPHONE_INSERT_FORM--', background_color=(INPUT_COLOR), font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
 
         [sg.Text('Mensagem', font=(TYPOGRAPHY_FONT, 14), pad=((0,0),(18,2)), background_color=DEFAULT_WHITE, expand_x=True)],
-        [sg.Multiline(key='--MESSAGE--', background_color=(INPUT_COLOR), pad=((0,0),(0,20)), no_scrollbar=True, expand_y=True, font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
+        [sg.Multiline(key='--MESSAGE_INSERT_FORM--', background_color=(INPUT_COLOR), pad=((0,0),(0,20)), no_scrollbar=True, expand_y=True, font=(TYPOGRAPHY_FONT, 12), do_not_clear=False, expand_x=True, border_width=0)],
 
         [
             sg.Push(),
             sg.Button(button_text='Salvar', button_color=(DEFAULT_TEXT_COLOR, SAVE_BUTTON_COLOR), mouseover_colors=HOVER_SAVE_BUTTON_COLOR, size=(15,2), border_width=0, key='--INSERT_SAVE--'),
-            sg.Button(button_text='Cancelar', button_color=(DEFAULT_TEXT_COLOR, CANCEL_BUTTON_COLOR), size=(15,2), border_width=0, key='--CANCEL--'),
+            sg.Button(button_text='Cancelar', button_color=(DEFAULT_TEXT_COLOR, CANCEL_BUTTON_COLOR), size=(15,2), border_width=0, key='--CANCEL_INSERT--'),
             sg.Push(),
         ],
     ]
@@ -70,18 +81,19 @@ insert_contact_form = [
 list_contacts_layout = [
 
     [sg.Text(text='Contatos:', font=(TYPOGRAPHY_FONT, 18, 'bold') )],
-    [sg.HorizontalSeparator(color=SAVE_BUTTON_COLOR)],
-
-    create_contact_card('Liedson dos Reis Pereira', "(xx) 9xxxx-xxxx", '--CARD1--')
+    [sg.Listbox(values=contact_list, size=(15, 6), key="-LISTA-", enable_events=True, expand_x=True, expand_y=True),],
 
 ]
 
 layout = [
 
     [
-        sg.Column(layout=list_contacts_layout, background_color=DEFAULT_WHITE, expand_x=True, expand_y=True,scrollable=True, vertical_scroll_only=True, sbar_background_color=SAVE_BUTTON_COLOR,sbar_relief=sg.RELIEF_FLAT, key='--CONTACT_LIST--'),
+        sg.Column(layout=list_contacts_layout, background_color=DEFAULT_WHITE, expand_x=True, expand_y=True, key='--CONTACT_LIST--'),
+
+        #Lado direito
         sg.Column(layout=main_layout, background_color=DEFAULT_WHITE, expand_x=True, expand_y=True, p=((50,50), (20,20)), key='--MAIN_MENU--'),
         sg.Column(layout=insert_contact_form, background_color=DEFAULT_WHITE, expand_x=True, expand_y=True, p=((50,50), (20,20)), key='--INSERT_CONTACT--', visible=False),
+        sg.Column(layout=update_contact_form, background_color=DEFAULT_WHITE, expand_x=True, expand_y=True, p=((50,50), (20,20)), key='--UPDATE_CONTACT--', visible=False),
     ]
 ]
 
@@ -89,6 +101,8 @@ janela = sg.Window("WhatsApp Automation", layout=layout, font=(TYPOGRAPHY_FONT),
 
 while True:
     eventos, valores = janela.read()
+
+    print(eventos, valores)
 
     if eventos in (sg.WIN_CLOSED, "sair"):
         break
@@ -99,15 +113,33 @@ while True:
         janela['--INSERT_CONTACT--'].update(visible=True)
 
     #Evento que retornar da tela de cadastro para o menu principal
-    if eventos in ['--CANCEL--']:
+    if eventos in ['--CANCEL_INSERT--']:
         
         resp = sg.Popup('Você deseja cancelar o cadastro? Esta ação é irreversivel!', no_titlebar=True, button_type=sg.POPUP_BUTTONS_OK_CANCEL, button_color=SAVE_BUTTON_COLOR, )
 
         if (resp in 'OK'):
             janela['--INSERT_CONTACT--'].update(visible=False)
-            janela['--NAME--'].update(value='')
-            janela['--CELLPHONE--'].update(value='')
-            janela['--MESSAGE--'].update(value='')
+            janela['--NAME_INSERT_FORM--'].update(value='')
+            janela['--CELLPHONE_INSERT_FORM--'].update(value='')
+            janela['--MESSAGE_INSERT_FORM--'].update(value='')
+            janela['--MAIN_MENU--'].update(visible=True)
+        
+        else:
+            pass
+
+    if eventos in ['--VER_CONTATO--']:
+        janela['--MAIN_MENU--'].update(visible=False)
+        janela['--UPDATE_CONTACT--'].update(visible=True)
+
+    if eventos in ['--CANCEL_UPDATE--']:
+        
+        resp = sg.Popup('Você deseja cancelar as alterações?', no_titlebar=True, button_type=sg.POPUP_BUTTONS_OK_CANCEL, button_color=SAVE_BUTTON_COLOR, )
+
+        if (resp in 'OK'):
+            janela['--UPDATE_CONTACT--'].update(visible=False)
+            janela['--NAME_UPDATE_FORM--'].update(value='')
+            janela['--CELLPHONE_UPDATE_FORM--'].update(value='')
+            janela['--MESSAGE_UPDATE_FORM--'].update(value='')
             janela['--MAIN_MENU--'].update(visible=True)
         
         else:
